f66333e5be7806fbecb6b0318b66887adfc0cd5d
slanceli <excitedooxx@gmail.com>
2023-06-05 15:31:23 +0800

    自动夜间模式

---
+++ static/js/custom.js
@@ -0,0 +1,9 @@
+// 自动夜间模式
+var hour = new Date().getHours();
+if (hour >= 19 || hour <= 6) {
+    $('body').attr('theme', 'dark');
+    window.localStorage.setItem('theme', 'dark');
+} else {
+    $('body').attr('theme', 'light');
+    window.localStorage.setItem('theme', 'light');
+}
